8d97f96680bd2d8cc98dfa39bc9cc9ec306c401d
oselpoderoso <oscataca@gmail.com>
2024-06-10 19:02:06 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 export default defineConfig({
   optimizeDeps: {
     include: ['vue-chartkick'],
   },
-  plugins: [
-    vue()
-  ]
+  plugins: [vue()],
+    
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
+  
 });
