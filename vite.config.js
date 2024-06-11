775aa63a7f8e74dd13fd0dc5989e13e2cafea50f
oselpoderoso <oscataca@gmail.com>
2024-06-10 19:27:06 -0500

    gh pages added

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     include: ['vue-chartkick'],
   },
   plugins: [vue()],
-  base: '/appgestion'
+  base: '/appgestion',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
